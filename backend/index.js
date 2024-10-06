51dcd6ef049395bc31a1968bc0ab69a35df7b15d
Pravin Falke <pravinfalke2003@gmail.com>
2024-10-06 17:47:34 +0530

    chore: changed backend app port

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { exec } = require('child_process');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
